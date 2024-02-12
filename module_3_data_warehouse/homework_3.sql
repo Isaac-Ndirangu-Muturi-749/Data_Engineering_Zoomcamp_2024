66e8b2a2b31255e997690b90f4fd1362b43e197e
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-13 00:24:19 +0300

    updated homework 3

--- module_3_data_warehouse/homework_3.sql
+++ module_3_data_warehouse/homework_3.sql
@@ -1,9 +1,12 @@
+
+# Homework preparation
 CREATE OR REPLACE EXTERNAL TABLE `direct-disk-412820.ny_taxi.green_taxi_2022`
 OPTIONS(
   format = 'PARQUET',
   uris = ['gs://mage_data_engineering_zoomcamp/green_taxi_data_2022/green_tripdata_2022-*.parquet']
 )
 
+# Homework preparation
 
 CREATE TABLE `direct-disk-412820.ny_taxi.green_taxi_2022_unpartitioned`
 AS
@@ -11,12 +14,12 @@ SELECT *
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022`;
 
 
-
+# Question 1
 SELECT COUNT(*) AS record_count
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022_unpartitioned`;
 
 
-
+# Question 2
 -- Query for External Table
 SELECT COUNT(DISTINCT PULocationID) AS distinct_PULocationIDs_external
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022`;
@@ -26,11 +29,13 @@ SELECT COUNT(DISTINCT PULocationID) AS distinct_PULocationIDs_table
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022_unpartitioned`;
 
 
+# Question 3
 SELECT COUNT(*)
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022_unpartitioned`
 WHERE fare_amount = 0;
 
 
+# Question 4
 CREATE TABLE direct-disk-412820.ny_taxi.green_taxi_2022_partitioned_clustered
 PARTITION BY DATE(lpep_pickup_datetime)
 CLUSTER BY PUlocationID
@@ -41,6 +46,7 @@ SELECT *
 FROM direct-disk-412820.ny_taxi.green_taxi_2022;
 
 
+# Question 5
 -- Size of the non-partitioned table
 SELECT
   table_id,
@@ -60,5 +66,6 @@ WHERE
   table_id = 'green_taxi_2022_partitioned_clustered';
 
 
+# Question 8
 SELECT count(*)
 FROM `direct-disk-412820.ny_taxi.green_taxi_2022_partitioned_clustered`;
